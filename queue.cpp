bf42a57889e5585094e66ae51c9b8cfbdd5c10aa
avantikaag03 <avantikaag03@gmail.com>
2021-03-04 08:25:51 +0530

    Added function for queue density

--- queue.cpp
+++ queue.cpp
@@ -11,12 +11,52 @@ using namespace cv;
 using namespace std;
 
 
+void queue_density(string video) {
+    Ptr<BackgroundSubtractor> pBackSub;
+    pBackSub = createBackgroundSubtractorMOG2();
+    VideoCapture capture(video);
+    if (!capture.isOpened()){
+        //error in opening the video input
+        cerr << "Unable to open the video frame" << endl;
+        return 0;
+    }
+
+    Mat frame, fgMask;
+
+    frame = imread("empty.jpg");
+    pBackSub->apply(frame, fgMask, 0);
+
+    while (true) {
+        capture >> frame;
+        if (frame.empty())
+            break;
+
+        //update the background model
+        pBackSub->apply(frame, fgMask, 0);
+        //absdiff(frame, background, fgMask);
+
+        //rectangle(frame, cv::Point(10, 2), cv::Point(100,20), cv::Scalar(255,255,255), -1);
+        stringstream ss;
+        ss << capture.get(CAP_PROP_POS_FRAMES);
+        string frameNumberString = ss.str();
+        cout << frameNumberString << " " <<;
+
+        //show the current frame and the fg masks
+        imshow("Frame", frame);
+        imshow("FG Mask", fgMask);
+
+        //get the input from the keyboard for quitting
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+}
 
 
 int main(int argc, char* argv[])
 {
     //create Background Subtractor objects
-    Ptr<BackgroundSubtractor> pBackSub;
+    /*Ptr<BackgroundSubtractor> pBackSub;
     pBackSub = createBackgroundSubtractorMOG2();
 
     VideoCapture capture(argv[1]);
@@ -55,7 +95,8 @@ int main(int argc, char* argv[])
         int keyboard = waitKey(30);
         if (keyboard == 'q' || keyboard == 27)
             break;
-    }
+    }*/
+    queue_density(argv[1]);
 
     return 0;
 }
