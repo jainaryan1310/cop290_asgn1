7893cf6700d3d08702290bd2de00fb3eceb351de
avantikaag03 <avantikaag03@gmail.com>
2021-03-04 09:15:04 +0530

    Included dependencies in header file

--- asgn1_sub1.h
+++ asgn1_sub1.h
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
 void LeftClick(int event, int x, int y, int flags, void* param);
 int getCropCoordinates(Mat grayImg);
 Mat warp(Mat grayImg);
