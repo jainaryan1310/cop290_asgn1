0e1e9180e2e2dd40b303f1aff3e021c74e06f132
avantikaag03 <avantikaag03@gmail.com>
2021-03-04 10:23:40 +0530

    Added code for crop, warp, density

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,4 @@
+#include "asgn1_sub1.h"
 #include <iostream>
 #include <sstream>
 #include <opencv2/imgcodecs.hpp>
@@ -24,25 +25,39 @@ void queue_density(string video) {
     Mat frame, fgMask;
 
     frame = imread("empty.jpg");
-    pBackSub->apply(frame, fgMask, 0);
+    Mat grayFrame;
+    cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
+
+    getCropCoordinates(grayFrame);
+    Mat warpedImg = warp(grayFrame);
+    Mat croppedImg = crop(warpedImg);
+
+    pBackSub->apply(croppedImg, fgMask, 0);
 
     while (true) {
         capture >> frame;
         if (frame.empty())
             break;
 
-        //update the background model
-        pBackSub->apply(frame, fgMask, 0);
-        //absdiff(frame, background, fgMask);
+        Mat grayFrame;
+        cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
 
-        //rectangle(frame, cv::Point(10, 2), cv::Point(100,20), cv::Scalar(255,255,255), -1);
+        Mat warpedImg = warp(grayFrame);
+        Mat croppedImg = crop(warpedImg);
+
+        //update the background model
+        pBackSub->apply(croppedImg, fgMask, 0);
+
+        float white = countNonZero(fgMask);
+        float total = fgMask.total();
+        float density = white/total;
+
         stringstream ss;
         ss << capture.get(CAP_PROP_POS_FRAMES);
         string frameNumberString = ss.str();
-        cout << frameNumberString << " " << endl;
+        cout << frameNumberString << " " << density << endl;
 
-        //show the current frame and the fg masks
-        imshow("Frame", frame);
+        //show the backfround subtraction output
         imshow("FG Mask", fgMask);
 
         //get the input from the keyboard for quitting
@@ -55,48 +70,6 @@ void queue_density(string video) {
 
 int main(int argc, char* argv[])
 {
-    //create Background Subtractor objects
-    /*Ptr<BackgroundSubtractor> pBackSub;
-    pBackSub = createBackgroundSubtractorMOG2();
-
-    VideoCapture capture(argv[1]);
-
-    if (!capture.isOpened()){
-        //error in opening the video input
-        cerr << "Unable to open the video frame" << endl;
-        return 0;
-    }
-
-    Mat frame, fgMask;
-
-    frame = imread("empty.jpg");
-    pBackSub->apply(frame, fgMask, 0);
-
-    while (true) {
-        capture >> frame;
-        if (frame.empty())
-            break;
-
-        //update the background model
-        pBackSub->apply(frame, fgMask, 0);
-        //absdiff(frame, background, fgMask);
-
-        //rectangle(frame, cv::Point(10, 2), cv::Point(100,20), cv::Scalar(255,255,255), -1);
-        stringstream ss;
-        ss << capture.get(CAP_PROP_POS_FRAMES);
-        string frameNumberString = ss.str();
-        cout << frameNumberString << endl;
-
-        //show the current frame and the fg masks
-        imshow("Frame", frame);
-        imshow("FG Mask", fgMask);
-
-        //get the input from the keyboard for quitting
-        int keyboard = waitKey(30);
-        if (keyboard == 'q' || keyboard == 27)
-            break;
-    }*/
     queue_density(argv[1]);
-
     return 0;
 }
